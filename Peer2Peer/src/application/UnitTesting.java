deeb25a4a71ce3b7f08a0285987ba10d53e4335b
Patrick NIYONGABO <patrickniyongabo@gmail.com>
2015-03-09 21:45:28 -0500

    added some comments to the unit testing

--- Peer2Peer/src/application/UnitTesting.java
+++ Peer2Peer/src/application/UnitTesting.java
@@ -9,6 +9,9 @@ public class UnitTesting {
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+		//will have to test non threaded code first
+		//before starting to create more threads & testing them 
 	}
+	
 
 }
